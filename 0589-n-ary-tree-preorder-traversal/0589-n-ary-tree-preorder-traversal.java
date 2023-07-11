c5ee4afd617d2ec47538701c8234304e16b54d87
urvashi2711 <106096180+urvashi2711@users.noreply.github.com>
2023-07-11 18:09:08 +0530

    Time: 3 ms (27.70%), Space: 44.6 MB (28.68%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -18,16 +18,26 @@ class Node {
 */
 
 class Solution {
-    private List<Integer> al = new ArrayList<>();
     public List<Integer> preorder(Node root) {
-       
-         if(root==null){
-            return al;
+        // To store the output array...
+        List<Integer> output = new ArrayList<Integer>();
+        // Base case: if the tree is empty...
+        if (root == null) return output;
+        // Create a stack of Node and push root to it...
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        // Traverse till stack is not empty...
+        while (!st.isEmpty()) {
+            // Pop a Node from the stack and add it to the output list...
+            Node node = st.pop();
+            output.add(node.val);
+            // Push all of the child nodes of the node into the stack from right to left...
+            // Push from right to left to get the right preorder traversal...
+            for (int idx = node.children.size() - 1; idx >= 0; idx--) {
+                Node child = node.children.get(idx);
+                st.push(child);
+            }
         }
-        al.add(root.val);
-        for(Node node: root.children)
-            preorder(node);
-        return al;
+        return output;      // Return the output...
     }
-   
 }
