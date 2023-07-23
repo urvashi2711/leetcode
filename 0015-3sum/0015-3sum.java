8522a6019929c84b1d97c702761430ac894a34da
urvashi2711 <106096180+urvashi2711@users.noreply.github.com>
2023-07-23 18:31:31 +0530

    Time: 682 ms (18.79%), Space: 50.7 MB (70.61%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,18 +3,18 @@ class Solution {
         Arrays.sort(nums);
         Set<List<Integer>> s=new HashSet<>();
         List<List<Integer>> output=new ArrayList<>();
-        int target=0;
+        
         for(int i=0;i<nums.length;i++){
             int j=i+1;
             int k=nums.length-1;
             while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(target==sum){
+                
+                if(nums[i]+nums[j]+nums[k]==0){
                     s.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;
                     k--;
                 }
-                else if(sum<target){
+                else if(nums[i]+nums[j]+nums[k]<0){
                     j++;
                     
                 }else k--;
