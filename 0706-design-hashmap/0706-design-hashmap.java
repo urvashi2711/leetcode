25cb1a8b5c4468aabe86e78c67263d48fe376da9
urvashi2711 <106096180+urvashi2711@users.noreply.github.com>
2023-07-24 19:25:24 +0530

    Time: 20 ms (85.95%), Space: 47.9 MB (68.26%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,20 +1,45 @@
+class ListNode {
+    int key, val;
+    ListNode next;
+    public ListNode(int key, int val, ListNode next) {
+        this.key = key;
+        this.val = val;
+        this.next = next;
+    }
+}
 class MyHashMap {
-    int arr[];
+    static final int size = 19997;
+    static final int mult = 12582917;
+    ListNode[] data;
     public MyHashMap() {
-        arr=new int[1000001];
-        Arrays.fill(arr,-1);
+        this.data = new ListNode[size];
     }
-    
-    public void put(int key, int value) {
-        arr[key]=value;
+    private int hash(int key) {
+        return (int)((long)key * mult % size);
     }
-    
+    public void put(int key, int val) {
+        remove(key);
+        int h = hash(key);
+        ListNode node = new ListNode(key, val, data[h]);
+        data[h] = node;
+    }
     public int get(int key) {
-        return arr[key];
+        int h = hash(key);
+        ListNode node = data[h];
+        for (; node != null; node = node.next)
+            if (node.key == key) return node.val;
+        return -1;
     }
-    
     public void remove(int key) {
-        arr[key]=-1;
+        int h = hash(key);
+        ListNode node = data[h];
+        if (node == null) return;
+        if (node.key == key) data[h] = node.next;
+        else for (; node.next != null; node = node.next)
+            if (node.next.key == key) {
+                node.next = node.next.next;
+                return;
+            }
     }
 }
 
