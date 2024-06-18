507cfb9acd53e75755276c9e697cf5d89fb78d12
urvashi2711 <106096180+urvashi2711@users.noreply.github.com>
2024-06-18 12:26:29 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (13.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,18 @@
 class Solution {
+    //top down Approach
+    private int[] dp = new int[31];
     public int fib(int n) {
-        if(n==0) return 0;
-        int[] dp = new int[n+1];
-        dp[0] =0;
-         dp[1] =1;
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+ dp[i-2];
-        }
+        if(n<=1) return n;
+        if(n==2) return 1;
+        memoize(n);
+        return dp[n];
+        
+    }
+    public int memoize(int n){
+        if(dp[n]!=0) return dp[n];
+        if(n<3) return 1;
+        else 
+            dp[n]= memoize(n-1)+memoize(n-2);
         return dp[n];
     }
 }
