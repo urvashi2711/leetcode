69f77f3e64b87c04a227b1b0a62a401056ac34c2
urvashi2711 <106096180+urvashi2711@users.noreply.github.com>
2024-07-19 20:29:10 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (54.20%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,25 +1,37 @@
+import java.util.LinkedList;
+
 class Solution {
     public boolean isHappy(int n) {
-       int slow=n;
-       int fast=n;
-        while(true){
-            slow=next(slow);
-            fast=next(next(fast));
-            if(slow==1 || fast==1){
-                return true;
-            }if(slow==fast){
-                return false;
-            }
-        }
+        
+        int slow = n;
+        int fast = n;
+            //while loop is not used here because initially slow and 
+            //fast pointer will be equal only, so the loop won't run.
+        do {
+             //slow moving one step ahead and fast moving two steps ahead
+
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
+              //if a cycle exists, then the number is not a happy number
+              //and slow will have a value other than 1
+
+        return slow == 1;
     }
-    public int next(int num){
-        int ans=0;
-        while(num>0){
-            int digit=num%10;
-            num=num/10;
-            ans=ans+digit*digit;
+    
+              //Finding the square of the digits of a number
+
+    public int square(int num) {
+        
+        int ans = 0;
+        
+        while(num > 0) {
+            int remainder = num % 10;
+            ans += remainder * remainder;
+            num /= 10;
         }
+        
         return ans;
     }
-   
 }
